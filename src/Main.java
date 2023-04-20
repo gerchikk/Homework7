eb7c54bffc4084211874214005280f3ecc169bf6
gerchikk <geraloi@yandex.ru>
2023-04-20 15:49:32 +0500

    Промежуток

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,96 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+    }
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    public static void task1() {
+        System.out.println("Задача 1");
+        int i = 0;
+        int total = 0;
+        int salary = 15_000;
+        int accumulation = 2_459_000;
+        while (total < accumulation) {
+            total = total + salary;
+            i++;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей");
         }
     }
+    public static void task2() {
+        System.out.println("Задача 2");
+        int i = 0;
+        while (i < 10) {
+            i = i + 1;
+            System.out.print(i + " ");
+        }
+        System.out.println(" ");
+        for (int k = 10; k >= 1; k = k - 1) {
+            System.out.print(k + " ");
+        }
+    }
+    public static void task3() {
+        System.out.println("Задача 3");
+        int born = 17;
+        int death = 8;
+        int population = 12_000_000;
+        int statistic = 1000;
+        int i = 0;
+        while (i < 10) {
+            population = population + population / statistic * (born - death);
+            i = i + 1;
+            System.out.println("Год " + i + ", численность населения составляет " + population);
+        }
+    }
+    public static void task4() {
+        System.out.println("Задача 4");
+        float procent = 0.07f;
+        int i = 0;
+        float total = 15_000;
+        int totalContribution = 12_000_000;
+        while (total < totalContribution) {
+            total = total + total * procent;
+            i++;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей");
+        }
+    }
+    public static void task5() {
+        System.out.println("Задача 5");
+        float procent = 0.07f;
+        int i = 0;
+        float total = 15_000f;
+        int totalContribution = 12_000_000;
+        while (total < totalContribution) {
+            total = total + total * procent;
+            i++;
+            if (i % 6 == 0) {
+                System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей");
+            }
+        }
+    }
+    public static void task6() {
+        System.out.println("Задача 6");
+        float procent = 0.07f;
+        int i = 0;
+        int monthInYear = 12;
+        int year = 9;
+        int totalMonth = monthInYear * year;
+        float total = 15_000f;
+        int finishContribution = 9;
+        while (i <= totalMonth) {
+            total = total + total * procent;
+            i++;
+            if (i % 6 == 0) {
+                System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей");
+            }
+        }
+    }
+
+
+
+
+
 }
